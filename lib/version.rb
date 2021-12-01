d1c791a5d98924efb1d2e8778a7a753da69be20e
Roderick Monje <rod@foveacentral.com>
2021-12-01 12:25:28 -0800

    fix: inch

--- lib/version.rb
+++ lib/version.rb
@@ -2,5 +2,6 @@
 
 # Ruby version of that old 90s chestnut, <BLINK>the web-site hit counter</BLINK>
 class HitCounter
+  # This gem's version
   VERSION = '0.1.4' unless defined?(HitCounter::VERSION)
 end
