325fb51372057c56ce7a1bd26b7c651e4a9423bb
Roderick Monje <rod@foveacentral.com>
2022-08-11 17:19:22 -0400

    conf: bumped

--- lib/version.rb
+++ lib/version.rb
@@ -3,5 +3,5 @@
 # Self-hosted Ruby version of that old 90s chestnut, <BLINK>the web-site hit counter</BLINK>
 class HitCounter
   # This gem's version
-  VERSION = '0.1.7' unless defined?(HitCounter::VERSION)
+  VERSION = '0.1.8' unless defined?(HitCounter::VERSION)
 end
